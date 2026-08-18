bc3a0a405d155eee0b211f33571aec8ef0729daa
mummyslovelyson <adutwumanita08@gmail.com>
2026-08-18 03:20:25 +0000

    Widen admin layout to max-w-screen-2xl, switch categories to table view

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -160,7 +160,7 @@ export default function AdminLayout() {
           </div>
         </header>
 
-        <main className="p-4 sm:p-6 lg:p-8 max-w-6xl mx-auto">
+        <main className="p-4 sm:p-6 lg:p-8 max-w-screen-2xl mx-auto">
           <Outlet />
         </main>
       </div>
