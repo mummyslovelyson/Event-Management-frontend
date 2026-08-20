ae541ceff920a07606e8db19e3a71f8f21fed83f
mummyslovelyson <adutwumanita08@gmail.com>
2026-08-20 21:26:37 +0000

    feat: restore categories management in organizer dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,7 @@ import MarketingPage from '@/pages/organizer/MarketingPage';
 import TeamPage from '@/pages/organizer/TeamPage';
 import WalletPage from '@/pages/organizer/WalletPage';
 import OrganizerSettingsPage from '@/pages/organizer/OrganizerSettingsPage';
+import OrganizerCategoriesPage from '@/pages/organizer/OrganizerCategoriesPage';
 
 // Admin pages
 import AdminDashboard from '@/pages/admin/AdminDashboard';
@@ -185,6 +186,7 @@ function AppRoutes() {
             <Route path="/organizer/team" element={<TeamPage />} />
             <Route path="/organizer/wallet" element={<WalletPage />} />
             <Route path="/organizer/support" element={<UserSupportPage />} />
+            <Route path="/organizer/categories" element={<OrganizerCategoriesPage />} />
             <Route path="/organizer/settings" element={<OrganizerSettingsPage />} />
           </Route>
 
